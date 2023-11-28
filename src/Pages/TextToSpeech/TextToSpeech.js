6accd34cdb2e6232c36a77ddb35052033d09e6e3
Annis Souames <souames.annis@gmail.com>
2023-11-28 21:44:47 +0100

    Added style

--- src/Pages/TextToSpeech/TextToSpeech.js
+++ src/Pages/TextToSpeech/TextToSpeech.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef , useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { ReactMic } from 'react-mic';
 import axios from 'axios';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
@@ -7,21 +7,22 @@ import Navigation from '../Shared/Navigation/Navigation';
 
 const TextToSpeech = () => {
 
-    // const {
-    //     transcript,
-    //     listening,
-    //     resetTranscript,
-    //     browserSupportsSpeechRecognition
-    //   } = useSpeechRecognition();
-    
+  // const {
+  //     transcript,
+  //     listening,
+  //     resetTranscript,
+  //     browserSupportsSpeechRecognition
+  //   } = useSpeechRecognition();
 
-    //   if (!browserSupportsSpeechRecognition) {
-    //     return <span>Browser doesn't support speech recognition.</span>;
-    //   }
 
-    const [isRecording, setRecording] = useState(false);
-    const [audioUrl, setAudioUrl] = useState(null);
-    const [language, setLanguage] = useState("en");
+  //   if (!browserSupportsSpeechRecognition) {
+  //     return <span>Browser doesn't support speech recognition.</span>;
+  //   }
+
+  const [isRecording, setRecording] = useState(false);
+  const [audioUrl, setAudioUrl] = useState(null);
+  const [currentVideo, setVideoUrl] = useState("panda.gif");
+  const [language, setLanguage] = useState("en");
 
   const startRecording = () => {
     setRecording(true);
@@ -41,48 +42,49 @@ const TextToSpeech = () => {
 
     const formData = new FormData();
     formData.append('file', recordedBlob.blob);
-    formData.append('lang', "en" );
-    formData.append('grp', "kid" );
+    formData.append('lang', "en");
+    formData.append('grp', "kid");
     console.log("API HIT")
     console.log("audioData API", recordedBlob.blob)
 
     try {
-     fetch('http://localhost:8000/answer', {
+      fetch('http://localhost:8000/answer', {
         method: 'POST',
         body: formData,
-      }).then(async(response) => {
+      }).then(async (response) => {
         const blob = await response.blob();
         const url = URL.createObjectURL(blob);
         setAudioUrl(url);
+        setVideoUrl("panda.gif")
       })
-      .then(data => console.log(typeof data) )
-      
+        .then(data => console.log(typeof data))
+
     } catch (error) {
       console.error('Error transcribing audio:', error);
     }
   };
 
 
-//   const sendAudioToBackend = async (audioBlob) => {
-//     const formData = new FormData();
-//     formData.append('file', audioData);
-//     console.log("API HIT")
-//     console.log("audioData API", audioData)
-//     try {
-//       const response = await fetch('http://localhost:8000/answer', {
-//         method: 'POST',
-//         body: formData,
-//       });
-      
-//       const data = await response.json();
-//       console.log(data);
-//     } catch (error) {
-//       console.error('Error transcribing audio:', error);
-//     }
-//   };
-  
+  //   const sendAudioToBackend = async (audioBlob) => {
+  //     const formData = new FormData();
+  //     formData.append('file', audioData);
+  //     console.log("API HIT")
+  //     console.log("audioData API", audioData)
+  //     try {
+  //       const response = await fetch('http://localhost:8000/answer', {
+  //         method: 'POST',
+  //         body: formData,
+  //       });
 
-    return (
+  //       const data = await response.json();
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.error('Error transcribing audio:', error);
+  //     }
+  //   };
+
+
+  return (
     <div>
       {/* <p>Microphone: {listening ? 'on' : 'off'}</p>
       <button onClick={SpeechRecognition.startListening}>Start</button>
@@ -90,40 +92,53 @@ const TextToSpeech = () => {
       <button onClick={resetTranscript}>Reset</button>
       <p>{transcript}</p>
     <p>sjdj</p> */}
-    <Navigation></Navigation>
-  
+      <Navigation></Navigation>
+
       <ReactMic
         record={isRecording}
-        className="sound-wave"
+        className="sound-wave mic"
         onStop={onStop}
         onData={onData}
         strokeColor="#000000"
-        backgroundColor="#FF4081"
+        backgroundColor="#64A8A6"
       />
-      <button onClick={startRecording} type="button">
-        Start
-      </button>
-      <button onClick={stopRecording} type="button">
-        Stop
-      </button>
-        {/* <h4>  Reply {
+      <div class="controls mx-5 d-flex justify-content-center mt-4">
+
+        <button href="#" class="button lightbg-blue clearfix mx-5" onClick={startRecording} >
+          <span>
+            Start</span>
+
+        </button>
+
+        <button href="#" class="button lightbg-blue clearfix" onClick={stopRecording} >
+          <span>
+            Stop
+          </span>
+
+        </button>
+      </div>
+      {/* <h4>  Reply {
                     audioData && audioData.map(transcribe => 
                         <p>{transcribe.answer}</p>
                         )
                 }
                 </h4> */}
-        {audioUrl && (
-        <audio autoPlay controls >
-          <source src={audioUrl}  type="audio/mp3" />
-          Your browser does not support the audio element.
-        </audio>
-        )}
-        
- 
+      {audioUrl && (
+        <div class="media">
+          <audio autoPlay controls >
+            <source src={audioUrl} type="audio/mp3" />
+            Your browser does not support the audio element.
+          </audio>
+          <img
+            src={currentVideo}
+          /></div>
+      )}
+
+
     </div>
 
-    
-    );
-  };
+
+  );
+};
 
 export default TextToSpeech;
